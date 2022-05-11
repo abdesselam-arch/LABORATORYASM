fb5fba7c4c7abd20a347c33ef9e81482c20ccd05
abdesselam-arch <abdesselam-arch>
2022-05-11 14:53:21 +0100

    fourth commit :

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -4,9 +4,9 @@
     <div class="col-md-6">
         <div class="card mx-4">
             <div class="card-body p-4">
-                <h1>{{ trans('panel.site_title') }}</h1>
+                <h1>{{ trans('LABORATORY ASM') }}</h1>
 
-                <p class="text-muted">{{ trans('global.reset_password') }}</p>
+                <p class="text-muted">{{ trans('reset password') }}</p>
 
                 <form method="POST" action="{{ route('password.request') }}">
                     @csrf
@@ -14,7 +14,7 @@
                     <input name="token" value="{{ $token }}" type="hidden">
 
                     <div class="form-group">
-                        <input id="email" type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autocomplete="email" autofocus placeholder="{{ trans('global.login_email') }}" value="{{ $email ?? old('email') }}">
+                        <input id="email" type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" required autocomplete="email" autofocus placeholder="{{ trans('login email') }}" value="{{ $email ?? old('email') }}">
 
                         @if($errors->has('email'))
                             <div class="invalid-feedback">
@@ -23,7 +23,7 @@
                         @endif
                     </div>
                     <div class="form-group">
-                        <input id="password" type="password" name="password" class="form-control" required placeholder="{{ trans('global.login_password') }}">
+                        <input id="password" type="password" name="password" class="form-control" required placeholder="{{ trans('login password') }}">
 
                         @if($errors->has('password'))
                             <div class="invalid-feedback">
@@ -32,13 +32,13 @@
                         @endif
                     </div>
                     <div class="form-group">
-                        <input id="password-confirm" type="password" name="password_confirmation" class="form-control" required placeholder="{{ trans('global.login_password_confirmation') }}">
+                        <input id="password-confirm" type="password" name="password_confirmation" class="form-control" required placeholder="{{ trans('login password confirmation') }}">
                     </div>
 
                     <div class="row">
                         <div class="col-12">
                             <button type="submit" class="btn btn-primary btn-block btn-flat">
-                                {{ trans('global.reset_password') }}
+                                {{ trans('reset password') }}
                             </button>
                         </div>
                     </div>
